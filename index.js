5098af2e314714e5f5a57b722f226701c414ee9b
Nick Bechtel <bechtel.nick@gmail.com>
2021-10-12 20:52:21 -0400

    commit tree b9886716b27acfb7bfbd3df2c548fb8d2a6690bc

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+///
+let random = Math.floor(Math.random() * 10) + 1;
+//
+let num3 = 16;
+let num4 = 4;
+let mod = num3 / num4;
+//
+let max = Math.max(10, 15, 20);
